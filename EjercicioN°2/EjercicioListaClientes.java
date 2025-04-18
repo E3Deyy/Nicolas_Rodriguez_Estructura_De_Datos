84a4ebbaf0b8ac0bd1a5b822697f44e593f95037
E3Deyy <nicolas15051@gmail.com>
2025-04-18 14:25:20 -0500

    EjercicioListaClientes.java

--- EjercicioN°2/EjercicioListaClientes.java
+++ EjercicioN°2/EjercicioListaClientes.java
@@ -10,5 +10,19 @@ class Cliente {
   public Cliente(int cedula, String nombre){
     this.cedula=cedula;
     this.nombre=nombre;
+    this.siguiente=null;
+    this.anterior=null;
+  }
+}
+//Clase que maneja la lista doble
+class listaDoble {
+  private Cliente cabeza;
+  private Cliente cola;
+
+  //Agregar cliente en la lista
+  public void insertarOrden(int cedula, String nombre){
+  Cliente nuevo = new Cliente(cedula, nombre);
+
+
   }
 }
