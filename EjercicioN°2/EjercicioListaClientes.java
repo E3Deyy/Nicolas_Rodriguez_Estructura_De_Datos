93536374e29d8913e87029e78b1c78f009a3d6aa
E3Deyy <nicolas15051@gmail.com>
2025-04-18 14:12:53 -0500

    EjercicioListaClientes.java

--- EjercicioN°2/EjercicioListaClientes.java
+++ EjercicioN°2/EjercicioListaClientes.java
@@ -1,1 +1,14 @@
+//Ejercicio Lista Doble de Clientes - Nicolás Rodríguez
 
+//Crearé la clase Nodo la cual será cliente
+class Cliente {
+  int cedula;
+  String nombre;
+  Cliente siguiente;
+  Cliente anterior;
+
+  public Cliente(int cedula, String nombre){
+    this.cedula=cedula;
+    this.nombre=nombre;
+  }
+}
