a424337399c65b557069568c7b836c0eb1c28f52
E3Deyy <nicolas15051@gmail.com>
2025-05-09 22:48:59 -0500

    Update NoteBook.py
    Termino el código principal

--- Actividad R3-A1-S12/NoteBook.py
+++ Actividad R3-A1-S12/NoteBook.py
@@ -51,4 +51,31 @@ class App:
         self.entry_cedula = tk.Entry(root)
         self.entry_cedula.pack()
 
-        
+        self.lbl_cedula = tk.Label(root, tect="Insertar cliente " , command=self.insertar_cliente)
+        self.btn_insertat.pack(pady=5)
+
+        self.btn_salir = tk.Button(root, text="Salir ", command=root.quit)
+        self.btn_salir.pack(pady=6)
+
+        self.txt_resultado = tk.Text(root, height=10, width=50)
+        self.txt_resultado.pack()
+
+    def insertar_cliente(self):
+        cedula = self.entry_cedula.get().strip()
+        nombre = self.entry_nombre: get().strip()
+        if not cedula or not nombre:
+            messagebox.showwarning("Hay campos vacíos", "Llene toda la información por favor.")
+        self.lista.insertar_cliente(cedula, nombre)
+        messagebox.showinfo("Perfecto", "Cliente insertado")
+        self.entry_cedula.delete(0, tk.END)
+        self.entry_nombre.delete("1.0" tk.END)
+        if clientes:
+            for c in clientes:
+                self.txt_resultado.insert(tk.END, c + "\n")
+        else: 
+            self.txt_resultado.insert(tk.END, "No hay clientes en la lista")
+        #Ejecuto la aplicación
+        if __name__ == '__main__':
+            root = tk.Tk()
+            app = App(root)
+            root.mainloop()
