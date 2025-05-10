d04d608d018d70be7f44110b6e7a7cc34d4bc29d
E3Deyy <nicolas15051@gmail.com>
2025-05-09 22:28:06 -0500

    Update NoteBook.py
    Creo la clase App que será la interfaz gráfica del programa

--- Actividad R3-A1-S12/NoteBook.py
+++ Actividad R3-A1-S12/NoteBook.py
@@ -23,3 +23,32 @@ class ListaCircularCliente:
             temp.siguiente = nuevo
             nuevo.siguiente = self.inicio
             self.inicio = nuevo #Se actualiza inicio al nodo nuevo
+
+def listar_clientes(self):
+    cliente = []
+    if self.inicio is None:
+            return clientes
+    actual = self.inicio
+    while True:
+        clientes.append(f"Cédula: {actual.cedula}, Nombre: {actual.nombre}")
+        if actual == self.inicio:
+            break
+        return clientes
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.tittle("Listar circular de clientes")
+        self.lista = ListaCircularCliente()
+
+        self.lbl_cedula = tk.Label(root, text="Cédula:")
+        self.lbl_cedula.pack()
+        self.entry_cedula = tk.Entry(root)
+        self.entry_cedula.pack()
+
+        self.lbl_nombre = tk.Label(root, text="Nombre:")
+        self.lbl_cedula.pack()
+        self.entry_cedula = tk.Entry(root)
+        self.entry_cedula.pack()
+
+        
