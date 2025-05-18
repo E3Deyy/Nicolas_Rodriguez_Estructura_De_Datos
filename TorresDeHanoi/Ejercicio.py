a7ad6c4ba865a2c1996e15d8c4e411b5c1fafebb
E3Deyy <nicolas15051@gmail.com>
2025-05-18 11:19:47 -0500

    Update Ejercicio.py
    Arreglo problemas con la identación en el programa

--- TorresDeHanoi/Ejercicio.py
+++ TorresDeHanoi/Ejercicio.py
@@ -20,42 +20,42 @@ class TorreDeHanoiGUI:
         #hago retraso en el algoritmo
         self.root.after(1000, lambda: self.torres_de_hanoi(self.n_discos, 'A', 'B', 'C'))
 
-  def crear_discos(self):
-      for i in range(self.n_discos, 0, -1):
-          ancho = 20 + i * 20
-          disco = self.canvas.create_rectangle(0, 0, ancho, 20, fill="black")
-          self.torres['A'].append(disco)
-      self.actualizar_dibujos()
+    def crear_discos(self):
+        for i in range(self.n_discos, 0, -1):
+            ancho = 20 + i * 20
+            disco = self.canvas.create_rectangle(0, 0, ancho, 20, fill="skyblue")
+            self.torres['A'].append(disco)
+        self.actualizar_dibujos()
       
-  def actualizar_dibujos(self):
-      for torre in ['A', 'B', 'C']:
-          x_base = self.coords_torres[torre]
-          for idx, disco in enumerate(reversed(self.torres[torre])):
-              ancho = self.canvas.coords(disco)[2]
-              x1 = x_base - ancho // 2
-              y1 = self.altura_base - (idx + 1) * 22
-              x2 = x_base + ancho
-              y2 = y1 + 20
-              self.canvas.coords(disco, x1, y1, x2, y2)
-      self.root.update()
-      time.sleep(0.5)
+    def actualizar_dibujos(self):
+        for torre in ['A', 'B', 'C']:
+            x_base = self.coords_torres[torre]
+            for idx, disco in enumerate(reversed(self.torres[torre])):
+                ancho = self.canvas.coords(disco)[2]
+                x1 = x_base - ancho // 2
+                y1 = self.altura_base - (idx + 1) * 22
+                x2 = x_base + ancho // 2
+                y2 = y1 + 20
+                self.canvas.coords(disco, x1, y1, x2, y2)
+        self.root.update()
+        time.sleep(0.5)
 
-def mover_disco(self, origen, destino):
-    disco = self.torres[origen].pop()
-    self.torres[destino].append(disco)
-    self.actualizar_dibujos()
+    def mover_disco(self, origen, destino):
+        disco = self.torres[origen].pop()
+        self.torres[destino].append(disco)
+        self.actualizar_dibujos()
 
-def torres_de_hanoi(self, n, origen, destino, ayudante):
-    if n ==1:
-        self.mover_disco(origen, destino)
-    else:
-        self.torres_de_hanoi(n - 1, origen, ayudante, destino)
-        self.mover_disco(origen, destino)
-        self.torres_de_hanoi(n - 1, ayudante, destino, origen)
+    def torres_de_hanoi(self, n, origen, destino, ayudante):
+        if n ==1:
+            self.mover_disco(origen, destino)
+        else:
+            self.torres_de_hanoi(n - 1, origen, ayudante, destino)
+            self.mover_disco(origen, destino)
+            self.torres_de_hanoi(n - 1, ayudante, destino, origen)
 
 #Programa main
 if __name__ == "__main__":
     discos = int(input("ingrese el numero de discos: "))
     root = tk.Tk()
     app = TorreDeHanoiGUI(root, discos)
-    riit.mainloop()
+    root.mainloop()
